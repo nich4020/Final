7eee2a1746b576d67d41ef10414c5f6b549e36dc
nich4020 <65908352+nich4020@users.noreply.github.com>
2026-01-16 14:12:44 -0500

    Add bread enemy projectiles and falling bread sprites
    Introduces a new 'breads' sprite class that spawns bread projectiles at random intervals. Also adds logic to occasionally spawn falling bread sprites, enhancing gameplay variety.

--- bread.py
+++ bread.py
@@ -34,7 +34,7 @@ class bread(oa.View):
         # ===================================
         # Sprites 
         self.player = Player(100, 200)
-        bread
+
         #================================== 
         # WALLS
         # floor{ 
@@ -46,13 +46,36 @@ class bread(oa.View):
 
         # =============================================
     def on_step(self):
-
+        if random.randint(0, 60) == 0:
+            breads(100, 300, group="enemy_projectiles")
         self.clear()
         self.scene.draw()
         self.draw_grid()
+        if random.randint(0, 1100) == 0:
+            oa.Sprite(10, int(self.player.x + random.randint(200, 2000)), get_asset_path("falling-Bread.png"), group="call")
 
 
+class breads(oa.Sprite):
 
+    def on_create(self):
+        slice = random.choice([get_asset_path("Bread.jpg"), get_asset_path("bread-pictures.jpg"), get_asset_path("milk-bread.jpg"), get_asset_path("sliced-bread.jpg"), get_asset_path("sliced-french-bread.jpg"), get_asset_path("Homemade-Bread.jpg"), get_asset_path("maxresdefault-3596983426.jpg"), get_asset_path("SUB.png"), get_asset_path("wheat-bread.jpg"), get_asset_path("whole-wheat-bread.jpg")])
+        jhgfd = slice
+        self.visual = jhgfd
+        self.scale = 0.5
+        self.layer = cc.Layers.MOVING_GAME_OBJECTS
+        self.change_x = 8
+        # vista_orb_sOUND = arcade.load_sound("Local/Unit 4/part1/sounds/Windows Shutdown.wav")
+        # vista_orb_play = arcade.play_sound(vista_orb_sOUND)
+
+
+    def on_step(self):
+        # If we've gone off screen, remove ourself from the game.
+        # It's important to remember this step, or otherwise the game will eventually
+        # slow down and it's trying to render a bunch of arrows off screen
+        if self.x == 1600:
+            self.kill() 
+        # # refs.cur_view.world_camera.point_in_view(int(self.x), int(self.y)) == False:
+        #     self.kill()
         
 
 oa.run(bread, title="moon gravity platformer game")
